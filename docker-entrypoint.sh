e505157d5a583b8034c0ee22a439af58555c8c1a
DylanWu <dw@watelier.cn>
2018-12-15 19:07:30 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -20,7 +20,7 @@ else
 	echo 'Generating the Hole of ' $DOMAIN ' and remember Big Brother is watching you'
 	curl https://raw.githubusercontent.com/wuzhongyi1105/caddy-sample/master/html.zip -O
 	unzip html.zip -d /var/www/html
-	printf "$DOMAIN\ngzip\nlog stdout\nerrors stdout\nroot /var/www/html\n \nforwardproxy {\n    basicauth $USER ${PWD}\n    ports     $PORT_80 $PORT_443\n    hide_ip\n    hide_via\n    probe_resistance\n    response_timeout 30\n    dial_timeout     30\n}" > /etc/caddy/Caddyfile
+	printf "$DOMAIN\ngzip\nlog stdout\nerrors stdout\nroot /var/www/html\n \nforwardproxy {\n    basicauth $USER $PASSWORD\n    ports     $PORT_80 $PORT_443\n    hide_ip\n    hide_via\n    probe_resistance\n    response_timeout 30\n    dial_timeout     30\n}" > /etc/caddy/Caddyfile
 fi
 
 /usr/bin/caddy --conf /etc/caddy/Caddyfile --log stdout --agree
